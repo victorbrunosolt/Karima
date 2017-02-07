76ea52b0fccdc263ad790d4301570b6db335cac1
victorbrunobotelho <victorbrunobotelho@hotmail.com>
2017-02-07 00:22:22 -0200

    mapa funcionando e trando rota, melhorar requisicoes assincronas estudar thead e melhorar carregamento das imagens.

--- ParseStarterProject/src/main/java/com/parse/starter/activity/CsaDetalhes.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/CsaDetalhes.java
@@ -1,20 +1,25 @@
 package com.parse.starter.activity;
 
-import android.Manifest;
+
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
+
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.widget.ArrayAdapter;
+
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -29,11 +34,10 @@ import com.parse.ParseQuery;
 import com.parse.starter.R;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 
-public class CsaDetalhes extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
-    private Toolbar toolbar;
+public class CsaDetalhes extends AppCompatActivity implements OnMapReadyCallback, LocationListener, DialogInterface{
+
     private String objectId;
     private TextView textViewNome;
     private TextView textViewDescricao;
@@ -42,19 +46,104 @@ public class CsaDetalhes extends AppCompatActivity implements OnMapReadyCallback
     private TextView textViewCapacidade;
     private ImageView imagemViewFotoCsa;
     private ParseObject csa;
-    private LocationManager lm;
-    private Location location;
-    private double longitude = -25.429675;
-    private double latitude = -49.271870;
+    private LocationManager locationManager;
+    private double latitudeCsa;
+    private double longitudeCsa;
+    private  GoogleMap map ;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_csa_detalhes);
         Intent intent = getIntent();
         objectId = intent.getStringExtra("objectId");
+
+        // metodos para recuperar dados da Activity
+
+        criatabs();
+        getComponentesTela();
         getCsa();
+        // mapa
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER,0,0,this);
+
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Toast.makeText(getApplicationContext(), "GPS ATIVADO: ", Toast.LENGTH_SHORT).show();
+        }else{
+            showGPSDisabledAlertToUser();
+        }
+
+
+
+        super.onResume();
+    }
+
+
+
+    // ------------CSA --------------------
+
+    private void getCsa() {
+
+        /*
+         Recupera csas
+        */
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("CSA");
+        query.getInBackground(objectId, new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+
+                // caso csa diferente de nula
+                if (e == null) {
+                    csa = object;
+                    //recuperando dados da csa
+                    String nome = (String) csa.get("NOME");
+                    String descricao = (String) csa.get("DESCRICAO");
+                    String endereco = (String) csa.get("ENDERECO");
+                    int preco = (int) csa.get("PRECO");
+                    int capacidade = (int) csa.get("CAPACIDADE");
+                    latitudeCsa =  csa.getParseGeoPoint("LOCALIZACAO").getLatitude();
+                    longitudeCsa = csa.getParseGeoPoint("LOCALIZACAO").getLongitude();
+
+
+
+
+                    //setando os dados na tela
+                    map.addMarker(new MarkerOptions().position(new LatLng(latitudeCsa, longitudeCsa)).title(nome));
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitudeCsa, longitudeCsa), 11));
+                    textViewNome.setText("Nome: " + nome);
+                    textViewDescricao.setText("Descrição: " + descricao);
+                    textViewEndereco.setText("Endereço: " + endereco);
+                    textViewPreco.setText("Preço: " + Integer.toString(preco) + "Reais");
+                    textViewCapacidade.setText("Capacidade: " + Integer.toString(capacidade));
+                    Picasso.with(getApplicationContext())
+                            .load(csa.getParseFile("IMAGEM").getUrl())
+                            .fit()
+                            .into(imagemViewFotoCsa);
+
+                } else {
+                    // something went wrong
+                }
+            }
+        });
+    }
+
+    private void getComponentesTela(){
 
         imagemViewFotoCsa = (ImageView) findViewById(R.id.imagem_fundo_Csa);
         textViewNome = (TextView) findViewById(R.id.nome_csa);
@@ -64,11 +153,13 @@ public class CsaDetalhes extends AppCompatActivity implements OnMapReadyCallback
         textViewCapacidade = (TextView) findViewById(R.id.capacidade_csa);
 
 
-        // mapa
+    }
 
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+    //----------------------------------TABS----------------------------------------
+
+
+    public void criatabs(){
 
 
         TabHost host = (TabHost) findViewById(R.id.tabHost);
@@ -92,71 +183,54 @@ public class CsaDetalhes extends AppCompatActivity implements OnMapReadyCallback
         spec.setIndicator("Avalições CSA");
         host.addTab(spec);
 
+
+
     }
 
-    private void getCsa() {
-
-        /*
-         Recupera csas
-        */
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("CSA");
-        query.getInBackground(objectId, new GetCallback<ParseObject>() {
-            public void done(ParseObject object, ParseException e) {
-
-                // caso csa diferente de nula
-                if (e == null) {
-                    csa = object;
-                    //recuperando dados da csa
-                    String nome = (String) csa.get("NOME");
-                    String descricao = (String) csa.get("DESCRICAO");
-                    String endereco = (String) csa.get("ENDERECO");
-                    int preco = (int) csa.get("PRECO");
-                    int capacidade = (int) csa.get("CAPACIDADE");
 
 
-                    //setando os dados na tela
-
-                    textViewNome.setText("Nome: " + nome);
-                    textViewDescricao.setText("Descrição: " + descricao);
-                    textViewEndereco.setText("Endereço: " + endereco);
-                    textViewPreco.setText("Preço: " + Integer.toString(preco) + "Reais");
-                    textViewCapacidade.setText("Capacidade: " + Integer.toString(capacidade));
-                    Picasso.with(getApplicationContext())
-                            .load(csa.getParseFile("IMAGEM").getUrl())
-                            .fit()
-                            .into(imagemViewFotoCsa);
-
-                } else {
-                    // something went wrong
-                }
-            }
-        });
-    }
+    //---------------------------------MAPS-----------------------------------------
 
     @Override
     public void onMapReady(GoogleMap map) {
-        if (lm != null) {
-            if (location != null) {
-                latitude = location.getLatitude();
-                longitude = location.getLongitude();
 
-            }
+        this.map = map;
+
+        try {
+            map.setMyLocationEnabled(true);
+            map.setTrafficEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(true);
+
+        }catch (SecurityException ex){
+
+            Toast.makeText(getApplicationContext(), "error: "+ex, Toast.LENGTH_SHORT).show();
         }
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            return;
-        }
 
-        map.setMyLocationEnabled(true);
-        map.setTrafficEnabled(true);
-
-        map.addMarker(new MarkerOptions().position(new LatLng(-25.443150, -49.238243)).title("Jardim Botânico"));
-        map.addMarker(new MarkerOptions().position(new LatLng(-25.389681, -49.231168)).title("Parque Bacaheri"));
-
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 11));
     }
 
+    private void showGPSDisabledAlertToUser(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS está desativado deseja ativar?")
+                .setCancelable(false)
+                .setPositiveButton("ativar GPS",
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Cancelar",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
 
     @Override
     public void onLocationChanged(Location location) {
@@ -177,5 +251,69 @@ public class CsaDetalhes extends AppCompatActivity implements OnMapReadyCallback
     public void onProviderDisabled(String provider) {
 
     }
+
+
+    @Override
+    public void cancel() {
+
+    }
+
+    @Override
+    public void dismiss() {
+
+    }
+
+
+    //--------------------------Classe AsyncTask----------------------------------
+
+    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+
+        private String resp;
+        ProgressDialog progressDialog;
+
+        @Override
+        protected String doInBackground(String... params) {
+            publishProgress("Sleeping..."); // Calls onProgressUpdate()
+            try {
+
+
+                int time = Integer.parseInt(params[0])*1000;
+
+                Thread.sleep(5);
+                resp = "Slept for " + params[0] + " seconds";
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            } catch (Exception e) {
+                e.printStackTrace();
+                resp = e.getMessage();
+            }
+            return resp;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            // execution of result of Long time consuming operation
+            progressDialog.dismiss();
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            progressDialog = ProgressDialog.show(CsaDetalhes.this,
+                    "ProgressDialog",
+                    "Wait for "+10+ " seconds");
+        }
+
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+            // Things to be done while execution of long running operation is in
+            // progress. For example updating ProgessDialog
+        }
+    }
 }
+
+
 
